688e674ba67a8281b6b9dfd8786b50af1e9b2f74
jnj3j3 <jnj22j33@gmail.com>
2026-08-13 03:57:04 +0000

    Initial commit

---
+++ src/main/java/com/moongcheap_backend/MoongCheapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moongcheap_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoongCheapBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoongCheapBackendApplication.class, args);
+    }
+
+}
