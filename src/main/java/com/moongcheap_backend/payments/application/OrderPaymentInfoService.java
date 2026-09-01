c8e2f527e44abc7623938c9189a3fe66ebc6d6ea
zzimzzim <kju1354@naver.com>
2026-09-01 17:17:41 +0900

    주문목록조회, 주문상세조회, 주문취소, 배송지입력 기능 작성 기능에 필요한 다른 도메인의 정보를 받아오는 서비스 작성 일부 enum 경로 수정

---
+++ src/main/java/com/moongcheap_backend/payments/application/OrderPaymentInfoService.java
@@ -0,0 +1,27 @@
+package com.moongcheap_backend.payments.application;
+
+import com.moongcheap_backend.order.domain.Orders;
+import com.moongcheap_backend.payments.domain.BrandPayMethod;
+import com.moongcheap_backend.payments.presentation.dto.OrderPaymentInfo;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderPaymentInfoService {
+
+    public OrderPaymentInfo getForOrder(Orders order) {
+        return new OrderPaymentInfo(
+            order.getPrice() * order.getSum(),
+            order.getDeliveryFee(),
+            order.getTotalAmount(),
+            formatPaymentMethod(order.getBrandPayMethod())
+        );
+    }
+
+    private String formatPaymentMethod(BrandPayMethod paymentMethod) {
+        if (paymentMethod == null) {
+            return null;
+        }
+
+        return paymentMethod.getProviderCode() + " " + paymentMethod.getMaskedNumber();
+    }
+}
